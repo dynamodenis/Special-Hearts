2cc33df30a0873dbe382066c624bb0ec9bc3ab63
Dynamo Denis <dmbugua66@gmail.com>
2020-03-24 20:52:47 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let shee= document.querySelector('.shee');
+
+// let timeline= new TimelineMax();
+
+// timeline.fromTo(shee,2,{height:'0%'}, {height:'70%'})
+
+$(document).ready(function(){
+    $('#btn').click(function(){
+        $('.main').show();
+    });
+});
